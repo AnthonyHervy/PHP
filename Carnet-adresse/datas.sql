b04431338f7f3afa3a1fe4c022cd39bb531990ed
Anthony Hervy <anthony.hervy@gmail.com>
2018-09-03 11:56:18 +0200

    Update datas.sql

--- Carnet-adresse/datas.sql
+++ Carnet-adresse/datas.sql
@@ -11,7 +11,7 @@ SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
 
 --
--- Base de données :  `calliweb`
+-- Base de données :  `adressbook`
 --
 
 -- --------------------------------------------------------
@@ -20,61 +20,42 @@ SET time_zone = "+00:00";
 -- Structure de la table `adress`
 --
 
-CREATE TABLE `adress` (
-  `id` int(11) NOT NULL,
-  `prefix` varchar(10) NOT NULL,
-  `lastname` varchar(70) NOT NULL,
-  `firstname` varchar(70) NOT NULL,
-  `adress` varchar(255) NOT NULL,
-  `adress2` varchar(255) NOT NULL,
-  `postalCode` varchar(15) NOT NULL,
-  `city` varchar(100) NOT NULL,
-  `country` varchar(50) NOT NULL,
-  `phoneNumber` int(25) NOT NULL,
-  `id_user` int(4) NOT NULL
+CREATE TABLE `adress`
+(
+  `id` int
+(11) NOT NULL,
+  `prefix` varchar
+(10) NOT NULL,
+  `lastname` varchar
+(70) NOT NULL,
+  `firstname` varchar
+(70) NOT NULL,
+  `adress` varchar
+(255) NOT NULL,
+  `adress2` varchar
+(255) NOT NULL,
+  `postalCode` varchar
+(15) NOT NULL,
+  `city` varchar
+(100) NOT NULL,
+  `country` varchar
+(50) NOT NULL,
+  `phoneNumber` int
+(25) NOT NULL,
+  `id_user` int
+(4) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
 -- Déchargement des données de la table `adress`
 --
 
-INSERT INTO `adress` (`id`, `prefix`, `lastname`, `firstname`, `adress`, `adress2`, `postalCode`, `city`, `country`, `phoneNumber`, `id_user`) VALUES
-(5, 'F', 'Dupont', 'Anne', 'Batiment C', '4 rue des oiseaux', '64100', 'Bayonne', 'France', 612345678, 11),
-(6, 'M', 'Test', 'Jean-test', '3 rue des developpeurs', '', '75000', 'Paris', 'France', 712345678, 11);
-
--- --------------------------------------------------------
-
---
--- Structure de la table `mails`
---
-
-CREATE TABLE `mails` (
-  `id` int(11) NOT NULL,
-  `mail` varchar(255) NOT NULL,
-  `key_mail` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
--- --------------------------------------------------------
-
---
--- Structure de la table `users`
---
-
-CREATE TABLE `users` (
-  `id` int(11) NOT NULL,
-  `username` varchar(50) NOT NULL,
-  `email` varchar(150) NOT NULL,
-  `password` varchar(255) NOT NULL,
-  `rank` tinyint(4) NOT NULL,
-  `active` tinyint(1) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
---
--- Déchargement des données de la table `users`
---
-
-INSERT INTO `users` (`id`, `username`, `email`, `password`, `rank`, `active`) VALUES
-(11, 'Admin', 'test-technique@calliweb.fr', '$2y$10$RrT1vWjur.VZ8ydQanFabuRaCDgwxP.jjkNRh0hkh/pC/ZiDPhS6a', 2, 1);
+INSERT INTO `adress` (`
+id`,
+`prefix
+`, `lastname`, `firstname`, `adress`, `adress2`, `postalCode`, `city`, `country`, `phoneNumber`, `id_user`) VALUES
+(7, 'M', 'Dupont', 'François', '4 rue des oiseaux', 'batiment C', '33000', 'Bordeaux', 'France', 612345678, 13),
+(8, 'F', 'Martin', 'Elisa', '21 Rue des oliviers', '', '33000', 'Bordeaux', 'France', 718294759, 13);
 
 --
 -- Index pour les tables déchargées
@@ -84,19 +65,8 @@ INSERT INTO `users` (`id`, `username`, `email`, `password`, `rank`, `active`) VA
 -- Index pour la table `adress`
 --
 ALTER TABLE `adress`
-  ADD PRIMARY KEY (`id`);
-
---
--- Index pour la table `mails`
---
-ALTER TABLE `mails`
-  ADD PRIMARY KEY (`id`);
-
---
--- Index pour la table `users`
---
-ALTER TABLE `users`
-  ADD PRIMARY KEY (`id`);
+ADD PRIMARY KEY
+(`id`);
 
 --
 -- AUTO_INCREMENT pour les tables déchargées
@@ -106,16 +76,5 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT pour la table `adress`
 --
 ALTER TABLE `adress`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
-
---
--- AUTO_INCREMENT pour la table `mails`
---
-ALTER TABLE `mails`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
-
---
--- AUTO_INCREMENT pour la table `users`
---
-ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+  MODIFY `id` int
+(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
